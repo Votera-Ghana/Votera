9841ee14e44108ecba88278f9454c03a7486ab64
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-27 09:10:28 +0000

    update app to include new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Elections from "./pages/Elections";
+import ElectionDetails from "./pages/ElectionDetails";
+import CandidateDetails from "./pages/CandidateDetails";
+
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/elections" element={<Elections />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/elections" element={<Elections />} />
+
+          <Route
+            path="/elections/:electionId"
+            element={<ElectionDetails />}
+          />
+
+          <Route
+            path="/elections/:electionId/candidate/:candidateId"
+            element={<CandidateDetails />}
+          />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
