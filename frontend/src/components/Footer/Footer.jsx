d601570738fef339313762b9660830314b640edb
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-27 09:33:37 +0000

    style: refine footer navigation

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,18 +1,42 @@
+import { Link, useNavigate } from "react-router-dom";
+
 import "./Footer.css";
+
 import logo from "../../assets/votera-logo.png";
 
 function Footer() {
+  const navigate = useNavigate();
+
+  const handleSectionNavigation = (sectionId) => {
+    navigate(`/#${sectionId}`);
+
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+
+      if (section) {
+        section.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }, 100);
+  };
+
   return (
     <footer className="footer">
       <div className="footer-container">
 
+        {/* Brand */}
         <div className="footer-main">
+
           <div className="footer-brand">
-            <img
-              src={logo}
-              alt="Votera"
-              className="footer-logo"
-            />
+            <Link to="/" className="footer-logo-link">
+              <img
+                src={logo}
+                alt="Votera"
+                className="footer-logo"
+              />
+            </Link>
 
             <p>
               Simple, secure, and transparent digital voting
@@ -20,31 +44,76 @@ function Footer() {
             </p>
           </div>
 
+          {/* Links */}
           <div className="footer-links">
+
+            {/* Platform */}
             <div className="footer-column">
               <h4>Platform</h4>
 
-              <a href="#home">Home</a>
-              <a href="#elections">Elections</a>
-              <a href="#how-it-works">How It Works</a>
+              <Link to="/">
+                Home
+              </Link>
+
+              <Link to="/elections">
+                Elections
+              </Link>
+
+              <button
+                type="button"
+                onClick={() =>
+                  handleSectionNavigation("how-it-works")
+                }
+              >
+                How It Works
+              </button>
             </div>
 
+            {/* Company */}
             <div className="footer-column">
               <h4>Company</h4>
 
-              <a href="#about">About</a>
-              <a href="#contact">Contact</a>
+              <button
+                type="button"
+                onClick={() =>
+                  handleSectionNavigation("about")
+                }
+              >
+                About
+              </button>
+
+              <button
+                type="button"
+                onClick={() =>
+                  handleSectionNavigation("contact")
+                }
+              >
+                Contact
+              </button>
             </div>
 
+            {/* Get Started */}
             <div className="footer-column">
               <h4>Get Started</h4>
 
-              <a href="#elections">Find an Election</a>
-              <a href="#how-it-works">How Voting Works</a>
+              <Link to="/elections">
+                Find an Election
+              </Link>
+
+              <button
+                type="button"
+                onClick={() =>
+                  handleSectionNavigation("how-it-works")
+                }
+              >
+                How Voting Works
+              </button>
             </div>
+
           </div>
         </div>
 
+        {/* Bottom */}
         <div className="footer-bottom">
           <span>
             © 2026 Votera. All rights reserved.
