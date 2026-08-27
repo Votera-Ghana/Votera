50af66f278be40e1337a5e53df0364b08a038c27
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-08-27 09:16:34 +0000

    update current Home.jsx:

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,13 @@
-import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import HowItWorks from "../components/HowItWorks/HowItWorks";
 import TrustSection from "../components/TrustSection/TrustSection";
-import Footer from "../components/Footer/Footer";
 
 function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <HowItWorks />
       <TrustSection />
-      <Footer />
     </>
   );
 }
